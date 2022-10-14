818c5eab4a7d7ccb199d30210f9d4a183b333201
Wesley Rodrigues <93356485+wesiley13@users.noreply.github.com>
2022-10-14 13:25:34 -0300

    first commit

---
+++ resources/views/templates/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    @stack('css')
+</head>
+<body>
+    <header class="bg-light shadow-sm border border-2 p-3">
+        @yield('header')
+    </header>
+
+    <main>
+        @yield('main')
+    </main>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.layout')
+
+@section('title', 'Login')
+
+@section('main')
+    <section class="my-4">
+        <h3>Tela inicial</h3>
+    </section>
+
+   
+@endsection
